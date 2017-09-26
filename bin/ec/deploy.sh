d8612efd2c1127e22174e56fa76190c1e0002dbf
Brian Rosner <brian@brosner.com>
2017-09-26 10:38:29 -0600

    removed CI debug

--- bin/ec/deploy.sh
+++ bin/ec/deploy.sh
@@ -6,6 +6,6 @@ set -eux
 INSTANCE=$1
 
 # @@@ set SHA after deployment succeeds
-ec --log-http --instance="$INSTANCE" instances env SHA="$CIRCLE_SHA1"
+ec --instance="$INSTANCE" instances env SHA="$CIRCLE_SHA1"
 ec --instance="$INSTANCE" deploy "$CIRCLE_SHA1"
 ec --instance="$INSTANCE" run web -- python manage.py migrate --noinput
