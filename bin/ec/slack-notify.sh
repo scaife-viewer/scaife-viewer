c855b48733c863b67da536929b65808aa5cdd0ba
Jake Wegner <jwegner@eldarion.com>
2017-09-27 10:31:39 -0500

    include links to GitHub and EC in Slack notification

--- bin/ec/slack-notify.sh
+++ bin/ec/slack-notify.sh
@@ -10,9 +10,10 @@ INSTANCE="$1"
 SHA_SHORT="${CIRCLE_SHA1:0:7}"
 
 AUTHOR_NAME="$(git show -s --format='%an' $SHA_SHORT)"
+EC_INSTANCE_URL="https://lk353.eu1.eldarioncloud.com/"
 
 # prepare Slack message payload
-MESSAGE="${AUTHOR_NAME} deployed \`${SHA_SHORT}\` to ${INSTANCE}."
+MESSAGE="${AUTHOR_NAME} deployed \`<https://github.com/eldarion-client/scaife-viewer/commit/${SHA_SHORT}|${SHA_SHORT}>\` to <${EC_INSTANCE_URL}|${INSTANCE}>."
 PAYLOAD_DATA="payload={\"channel\": \"#dev-feed\", \"username\": \"EC Deployments\", \"text\": \"${MESSAGE}\", \"icon_emoji\": \":lightning_cloud:\"}"
 
 # post to Slack
