bbf16c783d0a1c6acf7d3a44e17c8de120117b57
Brian Rosner <brian@brosner.com>
2017-10-06 15:51:20 -0600

    improved tracing and added requests spans

--- scaife_viewer/tracing.py
+++ scaife_viewer/tracing.py
@@ -9,7 +9,9 @@ import time
 from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 
+import opentracing
 import opentracing.ext.tags as ext_tags
+import wrapt
 from basictracer import BasicTracer
 from basictracer.recorder import SpanRecorder
 from google.auth.transport.requests import AuthorizedSession
@@ -121,14 +123,16 @@ def parse_http_headers(request):
     return headers
 
 
-class OpenTracingMiddleware(MiddlewareMixin):
+class OpenTracingMiddleware:
 
     def __init__(self, get_response=None):
         self.tracer = GCPTracer()
         self.get_response = get_response
         self.current_spans = {}
 
-    def process_view(self, request, view_func, view_args, view_kwargs):
+        install_requests_patch(self.tracer)
+
+    def __call__(self, request):
         headers = parse_http_headers(request)
         tags = {
             ext_tags.SPAN_KIND: ext_tags.SPAN_KIND_RPC_SERVER,
@@ -136,7 +140,7 @@ class OpenTracingMiddleware(MiddlewareMixin):
             ext_tags.HTTP_METHOD: request.method,
         }
         span = None
-        operation_name = f"view:{view_func.__name__}"
+        operation_name = request.path
         try:
             # check http headers to see if we should extract a parent span
             span_ctx = self.tracer.extract(Format.HTTP_HEADERS, carrier=headers)
@@ -147,17 +151,78 @@ class OpenTracingMiddleware(MiddlewareMixin):
             )
         except (InvalidCarrierException, SpanContextCorruptedException) as e:
             span = self.tracer.start_span(operation_name=operation_name, tags=tags)
-        self.current_spans[request] = span
-
-    def process_response(self, request, response):
-        span = self.current_spans.pop(request, None)
-        if span is not None:
-            span.set_tag(ext_tags.HTTP_STATUS_CODE, response.status_code)
-            span.finish()
+        with span_in_context(span):
+            response = self.get_response(request)
+        span.set_tag(ext_tags.HTTP_STATUS_CODE, response.status_code)
+        span.finish()
         return response
 
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        span = get_current_span()
+        span.set_tag("django.view", view_func.__name__)
+
     def process_exception(self, request, exc):
-        span = self.current_spans.pop(request, None)
+        span = get_current_span()
         if span is not None:
             # span.set_tag(ext_tags.ERROR, str(exc))
             span.finish()
+
+
+class RequestContext(object):
+
+    __slots__ = ("span",)
+
+    def __init__(self, span):
+        self.span = span
+
+
+class RequestContextManager:
+
+    _state = threading.local()
+    _state.context = None
+
+    @classmethod
+    def current_context(cls):
+        return getattr(cls._state, "context", None)
+
+    def __init__(self, context):
+        if isinstance(context, opentracing.Span):
+            self._context = RequestContext(span=context)
+        else:
+            self._context = context
+
+    def __enter__(self):
+        self._prev_context = self.__class__.current_context()
+        self.__class__._state.context = self._context
+        return self._context
+
+    def __exit__(self, *_):
+        self.__class__._state.context = self._prev_context
+        self._prev_context = None
+        return False
+
+
+def get_current_span():
+    context = RequestContextManager.current_context()
+    return context.span if context else None
+
+
+def span_in_context(span):
+    context = RequestContext(span)
+    return RequestContextManager(context)
+
+
+def install_requests_patch(tracer):
+    import requests.adapters
+
+    @wrapt.decorator
+    def trace(wrapped, instance, args, kwargs):
+        span = tracer.start_span(
+            operation_name="requests",
+            child_of=get_current_span(),
+        )
+        with span:
+            resp = wrapped(*args, **kwargs)
+        return resp
+
+    requests.adapters.HTTPAdapter.send = trace(requests.adapters.HTTPAdapter.send)
