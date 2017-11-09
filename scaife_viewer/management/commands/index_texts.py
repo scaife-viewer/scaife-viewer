b75a0be195aed2c208c086276ad43725da953f1b
Brian Rosner <brian@brosner.com>
2017-11-09 16:45:35 -0700

    implemented more flexibility in indexer

--- scaife_viewer/management/commands/index_texts.py
+++ scaife_viewer/management/commands/index_texts.py
@@ -3,7 +3,7 @@ import json
 import timeit
 from decimal import Decimal
 from functools import partial
-from itertools import zip_longest
+from itertools import chain, zip_longest
 from operator import attrgetter
 from typing import Iterable
 
@@ -19,42 +19,54 @@ class Command(BaseCommand):
 
     help = "Indexes all texts in Elasticsearch"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--workers",
+            type=int,
+            default=None,
+        )
+        parser.add_argument(
+            "--dry-run",
+            action="store_true",
+            default=False,
+        )
+        parser.add_argument("--urn-filter")
+        parser.add_argument("--chunk-size", type=int, default=100)
+
     def handle(self, *args, **options):
-        cts.TextInventory.load()
-        print("text inventory loaded")
-        num_workers = None
-        dry_run = True
+        num_workers = options["workers"]
+        dry_run = options["dry_run"]
+        urn_filter = options["urn_filter"]
 
         if not dry_run:
             create_es_index()
 
-        with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as executor:
-            tasks = executor.map(
-                partial(index_text_chunk, dry_run=dry_run),
-                chunker(self.urns(), 100),
-                chunksize=100,
-            )
-            for completed in tasks:
-                for urn, elapsed in completed:
-                    duration = Decimal.from_float(elapsed).quantize(Decimal("0.00"))
-                    print(f"indexed {urn} ({duration}s)")
+        cts.TextInventory.load()
+        print("Text inventory loaded")
 
-    def texts(self):
+        with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as executor:
+            if urn_filter:
+                print(f"Applying URN filter: {urn_filter}")
+            urns = list(chain.from_iterable(executor.map(
+                passage_urns_from_text,
+                self.texts(urn_filter),
+                chunksize=100,
+            )))
+            print(f"Indexing {len(urns)} passages")
+            list(executor.map(
+                partial(index_text_chunk, dry_run=dry_run),
+                chunker(urns, options["chunk_size"]),
+                chunksize=1,
+            ))
+
+    def texts(self, urn_filter):
         ti = cts.text_inventory()
         for text_group in ti.text_groups():
             for work in text_group.works():
                 for text in work.texts():
+                    if urn_filter and not str(text.urn).startswith(urn_filter):
+                        continue
                     yield text
-
-    def urns(self):
-        for text in self.texts():
-            try:
-                toc = text.toc()
-            except Exception as e:
-                print(f"{text.urn} toc error: {e}")
-                continue
-            for node in PreOrderIter(toc.root, filter_=attrgetter("is_leaf")):
-                yield f"{text.urn}:{node.reference}"
 
 
 def chunker(iterable, n):
@@ -63,78 +75,105 @@ def chunker(iterable, n):
         yield [item for item in chunk if item is not None]
 
 
+def passage_urns_from_text(text):
+    urns = []
+    try:
+        toc = text.toc()
+    except Exception as e:
+        print(f"{text.urn} toc error: {e}")
+    else:
+        for node in PreOrderIter(toc.root, filter_=attrgetter("is_leaf")):
+            urns.append(f"{text.urn}:{node.reference}")
+    return urns
+
+
 def create_es_index():
     index_name = "scaife-viewer"
     doc_type = "text"
 
-    payload = {
-        "mappings": {
-            doc_type: {
-                "properties": {
-                    "text": {
-                        "type": "text",
-                        "term_vector": "with_positions_offsets",
-                        "store": True,
-                        "analyzer": "fulltext_analyzer",
+    r = requests.get(**es_req_kwargs(f"/{index_name}"))
+    if r.status_code == 404:
+        payload = {
+            "mappings": {
+                doc_type: {
+                    "properties": {
+                        "content": {
+                            "type": "text",
+                            "term_vector": "with_positions_offsets",
+                            "store": True,
+                            "analyzer": "fulltext_analyzer",
+                        },
                     },
                 },
             },
-        },
-        "settings": {
-            "index": {
-                "number_of_shards": 5,
-                "number_of_replicas": 0,
-            },
-            "analysis": {
-                "analyzer": {
-                    "fulltext_analyzer": {
-                        "type": "custom",
-                        "tokenizer": "standard",  # https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-standard-tokenizer.html
+            "settings": {
+                "index": {
+                    "number_of_shards": 5,
+                    "number_of_replicas": 0,
+                },
+                "analysis": {
+                    "analyzer": {
+                        "fulltext_analyzer": {
+                            "type": "custom",
+                            "tokenizer": "standard",  # https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-standard-tokenizer.html
+                        },
                     },
                 },
             },
-        },
-    }
-    r = requests.put(**es_req_kwargs(
-        f"/{index_name}",
-        data=json.dumps(payload),
-    ))
-    r.raise_for_status()
+        }
+        r = requests.put(**es_req_kwargs(
+            f"/{index_name}",
+            data=json.dumps(payload),
+        ))
+        r.raise_for_status()
 
 
 def index_text_chunk(chunk: Iterable[str], dry_run: bool):
-    resolver = cts.default_resolver()
-    urns = []
     index_name = "scaife-viewer"
     doc_type = "text"
+    docs = []
 
     for urn in chunk:
-        start_time = timeit.default_timer()
         passage = cts.passage(urn)
-        # to avoid lru_cache
-        textual_node = resolver.getTextualNode(passage.text.urn, subreference=str(passage.reference))
-        raw_text = textual_node.export(exclude=["tei:teiHeader"])
         doc = {
             "urn": urn,
-            "label": passage.text.label,
-            "description": passage.text.description,
-            "text": raw_text,
+            "text": {
+                "urn": str(passage.text.urn),
+                "label": passage.text.label,
+                "description": passage.text.description,
+            },
+            "reference": str(passage.reference),
+            "content": passage.textual_node().export(exclude=["tei:teiHeader"]),
         }
-        if not dry_run:
-            r = requests.put(
-                **es_req_kwargs(
-                    f"/{index_name}/{doc_type}/{urn}",
-                    data=json.dumps(doc).encode("utf-8"),
-                    headers={
-                        "Content-Type": "application/json",
-                    },
-                ),
-            )
-            r.raise_for_status()
-        elapsed = timeit.default_timer() - start_time
-        urns.append((urn, elapsed))
+        docs.append(doc)
 
-    return urns
+    if not dry_run:
+        lines = []
+        for doc in docs:
+            lines.extend([
+                json.dumps({
+                    "index": {
+                        "_index": index_name,
+                        "_type": doc_type,
+                        "_id": doc["urn"],
+                    },
+                }),
+                json.dumps(doc),
+            ])
+        r = requests.post(
+            **es_req_kwargs(
+                "/_bulk",
+                data="\n".join(lines).encode("utf-8"),
+                headers={
+                    "Content-Type": "application/x-ndjson",
+                },
+            ),
+        )
+        if r.status_code == 400:
+            print(r.json())
+        r.raise_for_status()
+
+    # print(f"Indexed {len(docs)} passages")
 
 
 def es_req_kwargs(path, **kwargs):
