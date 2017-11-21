2618e61dd1e0fb07b072c81e827e0a500e9289dd
Brian Rosner <brian@brosner.com>
2017-11-21 14:02:33 -0700

    refactored indexer

--- scaife_viewer/management/commands/index_passages.py
+++ scaife_viewer/management/commands/index_passages.py
@@ -1,24 +1,10 @@
 import concurrent.futures
-import contextlib
-import cProfile
-import json
-import os
-import pstats
 import time
-from collections import deque
 from decimal import Decimal
-from functools import partial
-from itertools import chain, islice, zip_longest
-from operator import attrgetter
-from typing import Iterable
 
-import google.cloud.pubsub
-import requests
-from anytree.iterators import PreOrderIter
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from ... import cts
+from ...indexer import Indexer, DirectPusher, PubSubPusher
 
 
 class Command(BaseCommand):
@@ -26,231 +12,34 @@ class Command(BaseCommand):
     help = "Indexes passages in Elasticsearch"
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            "--max-workers",
-            type=int,
-            default=None,
-        )
-        parser.add_argument(
-            "--dry-run",
-            action="store_true",
-            default=False,
-        )
+        parser.add_argument("--max-workers", type=int, default=None)
+        parser.add_argument("--dry-run", action="store_true", default=False)
         parser.add_argument("--urn-prefix")
         parser.add_argument("--chunk-size", type=int, default=100)
         parser.add_argument("--limit", type=int, default=None)
         parser.add_argument("--delete-index", action="store_true", default=False)
-        parser.add_argument("--pusher", type=str, default="elasticsearch")
+        parser.add_argument("--pusher", type=str, default="direct")
 
     def handle(self, *args, **options):
-        max_workers = options["max_workers"]
-        dry_run = options["dry_run"]
-        urn_prefix = options["urn_prefix"]
-        limit = options["limit"]
-        delete_index = options["delete_index"]
-
-        if options["pusher"] == "elasticsearch":
-            pusher = ElasticsearchPushMode()
-        if options["pusher"] == "pubsub":
-            pusher = GooglePubSubPushMode()
-
-        with CodeTimer() as timer:
-            self.index_texts(
-                max_workers,
-                dry_run,
-                urn_prefix,
-                limit,
-                options["chunk_size"],
-                delete_index,
-                pusher,
-            )
+        executor = concurrent.futures.ProcessPoolExecutor(max_workers=options["max_workers"])
+        if options["pusher"] == "direct":
+            pusher = DirectPusher()
+        elif options["pusher"] == "pubsub":
+            pusher = PubSubPusher()
+        indexer = Indexer(
+            executor, pusher,
+            urn_prefix=options["urn_prefix"],
+            chunk_size=options["chunk_size"],
+            limit=options["limit"],
+            dry_run=options["dry_run"],
+        )
+        with Timer() as timer:
+            indexer.index()
         elapsed = timer.elapsed.quantize(Decimal("0.00"))
         print(f"Finished in {elapsed}s")
 
-    def index_texts(self, num_workers, dry_run, urn_prefix, limit, chunk_size, delete_index, pusher):
-        # if not dry_run:
-        #     if delete_index:
-        #         delete_es_index()
-        #     create_es_index()
-        cts.TextInventory.load()
-        print("Text inventory loaded")
-        with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as executor:
-            if urn_prefix:
-                print(f"Applying URN prefix filter: {urn_prefix}")
-            urns = chain.from_iterable(executor.map(passage_urns_from_text, self.texts(urn_prefix), chunksize=100))
-            if limit:
-                urns = islice(urns, limit)
-            urns = list(urns)
-            print(f"Indexing {len(urns)} passages")
-            indexer = partial(index_text_chunk, dry_run=dry_run, pusher=pusher)
-            consume(executor.map(indexer, chunker(urns, chunk_size)))
 
-    def texts(self, urn_prefix):
-        ti = cts.text_inventory()
-        for text_group in ti.text_groups():
-            for work in text_group.works():
-                for text in work.texts():
-                    if urn_prefix and not str(text.urn).startswith(urn_prefix):
-                        continue
-                    yield text
-
-
-def consume(it):
-    deque(it, maxlen=0)
-
-
-def chunker(iterable, n):
-    args = [iter(iterable)] * n
-    for chunk in zip_longest(*args, fillvalue=None):
-        yield [item for item in chunk if item is not None]
-
-
-def passage_urns_from_text(text):
-    urns = []
-    try:
-        toc = text.toc()
-    except Exception as e:
-        print(f"{text.urn} toc error: {e}")
-    else:
-        for node in PreOrderIter(toc.root, filter_=attrgetter("is_leaf")):
-            urns.append(f"{text.urn}:{node.reference}")
-    return urns
-
-
-def es_index_exists():
-    index_name = "scaife-viewer"
-    r = requests.get(**es_req_kwargs(f"/{index_name}"))
-    return r.ok
-
-
-def delete_es_index():
-    index_name = "scaife-viewer"
-    if es_index_exists():
-        requests.delete(**es_req_kwargs(f"/{index_name}"))
-
-
-def create_es_index():
-    index_name = "scaife-viewer"
-    doc_type = "text"
-
-    if not es_index_exists():
-        payload = {
-            "mappings": {
-                doc_type: {
-                    "properties": {
-                        "urn": {
-                            "type": "keyword",
-                        },
-                        "text_group": {
-                            "type": "keyword",
-                        },
-                        "work": {
-                            "type": "keyword",
-                        },
-                        "content": {
-                            "type": "text",
-                            "term_vector": "with_positions_offsets",
-                            "store": True,
-                            "analyzer": "fulltext_analyzer",
-                        },
-                    },
-                },
-            },
-            "settings": {
-                "index": {
-                    "number_of_shards": 5,
-                    "number_of_replicas": 0,
-                },
-                "analysis": {
-                    "analyzer": {
-                        "fulltext_analyzer": {
-                            "type": "custom",
-                            "tokenizer": "icu_tokenizer",
-                            "filter": [
-                                "icu_folding",
-                            ],
-                        },
-                    },
-                },
-            },
-        }
-        r = requests.put(**es_req_kwargs(
-            f"/{index_name}",
-            data=json.dumps(payload),
-            headers={"Content-Type": "application/json"},
-        ))
-        if r.status_code >= 400 and r.status_code < 500:
-            raise Exception(r.json())
-        r.raise_for_status()
-
-
-def log(msg):
-    pid = os.getpid()
-    print(f"[pid={pid}] {msg}")
-
-
-class ElasticsearchPushMode:
-
-    def __init__(self):
-        pass
-
-    def push(self):
-        pass
-
-
-class GooglePubSubPushMode:
-
-    def __init__(self):
-        self.topic_path = f"projects/ec-perseus/topics/indexer-dev-documents"
-
-    @property
-    def publisher(self):
-        if not hasattr(self, "_publisher"):
-            self._publisher = google.cloud.pubsub.PublisherClient()
-        return self._publisher
-
-    def push(self, doc):
-        self.publisher.publish(self.topic_path, json.dumps(doc).encode("utf-8"))
-
-    def __getstate__(self):
-        s = self.__dict__.copy()
-        if "_publisher" in s:
-            del s["_publisher"]
-        return s
-
-
-def index_text_chunk(chunk: Iterable[str], dry_run: bool, pusher):
-    for urn in chunk:
-        try:
-            passage = cts.passage(urn)
-        except cts.PassageDoesNotExist:
-            print(f"Passage {urn} does not exist")
-            continue
-        except Exception as e:
-            print(f"Error {e}")
-            continue
-        doc = {
-            "urn": urn,
-            "text_group": str(passage.text.urn.upTo(cts.URN.TEXTGROUP)),
-            "work": str(passage.text.urn.upTo(cts.URN.WORK)),
-            "text": {
-                "urn": str(passage.text.urn),
-                "label": passage.text.label,
-                "description": passage.text.description,
-            },
-            "reference": str(passage.reference),
-            "content": passage.content,  # CPU intensive
-        }
-        if not dry_run:
-            pusher.push(doc)
-
-
-def es_req_kwargs(path, **kwargs):
-    kwargs["url"] = f"{settings.ELASTICSEARCH_URL}{path}"
-    return kwargs
-
-
-class CodeTimer:
+class Timer:
 
     def __enter__(self):
         self.start = time.perf_counter()
@@ -258,13 +47,3 @@ class CodeTimer:
 
     def __exit__(self, typ, value, traceback):
         self.elapsed = Decimal.from_float(time.perf_counter() - self.start)
-
-
-@contextlib.contextmanager
-def profile(*args, **kwargs):
-    profile = cProfile.Profile(*args, **kwargs)
-    profile.enable()
-    yield
-    profile.disable()
-    ps = pstats.Stats(profile)
-    ps.sort_stats("time", "cumulative").print_stats(.1)
