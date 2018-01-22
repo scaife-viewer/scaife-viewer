2d17c66b6fafd398caa42c2e9f07c6b2b300660d
Brian Rosner <brian@brosner.com>
2018-01-22 16:55:45 -0700

    implemented first pass on text mode

--- static/src/js/reader/token.vue
+++ static/src/js/reader/token.vue
@@ -22,6 +22,7 @@ export default {
   },
   render(h) {
     let selected = false;
+    let clickable = false;
     const {
       t, idx,
       $parent: p,
@@ -29,7 +30,10 @@ export default {
     } = this;
     const { visible } = p;
     if (visible) {
-      const { annotations, annotationChange } = store.state.reader;
+      const { textMode, annotations, annotationChange } = store.state.reader;
+      if (textMode === 'clickable') {
+        clickable = true;
+      }
       if (annotations.has(idx)) {
         ({ selected } = annotations.get(idx));
       }
@@ -39,20 +43,22 @@ export default {
       {
         class: [
           t,
-          { selected },
+          { c: clickable, selected },
         ],
         on: {
           click(e) {
-            if (e.metaKey) {
-              if (selected) {
-                store.dispatch('reader/setSelectedToken', { token: null });
+            if (clickable) {
+              if (e.metaKey) {
+                if (selected) {
+                  store.dispatch('reader/setSelectedToken', { token: null });
+                }
+              } else if (e.shiftKey) {
+                store.dispatch('reader/selectTokenRange', { token: idx });
+              } else {
+                store.dispatch('reader/setSelectedToken', { token: idx });
               }
-            } else if (e.shiftKey) {
-              store.dispatch('reader/selectTokenRange', { token: idx });
-            } else {
-              store.dispatch('reader/setSelectedToken', { token: idx });
+              e.stopPropagation();
             }
-            e.stopPropagation();
           },
         },
       },
---
+++ static/src/js/reader/widgets/text-mode.vue
@@ -0,0 +1,34 @@
+<template>
+  <widget>
+    <span slot="header">Text Mode</span>
+    <div slot="body">
+      <a :class="[{active: textMode === 'browser'}]" @click.prevent="changeTextMode('browser')">Browser</a>
+      <a :class="[{active: textMode === 'clickable'}]" @click.prevent="changeTextMode('clickable')">Clickable</a>
+    </div>
+  </widget>
+</template>
+
+<script>
+import store from '../../store';
+import widget from '../widget';
+
+export default {
+  store,
+  computed: {
+    textMode() {
+      return this.$store.state.reader.textMode;
+    },
+  },
+  methods: {
+    changeTextMode(mode) {
+      this.$store.dispatch('reader/setSelectedToken', { token: null })
+        .then(() => {
+          this.$store.commit('reader/setTextMode', { mode });
+        });
+    },
+  },
+  components: {
+    widget,
+  },
+};
+</script>
