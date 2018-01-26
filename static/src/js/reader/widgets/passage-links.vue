cd874aa4f992d02902ec8afa176e8c4f0783a5c8
Brian Rosner <brian@brosner.com>
2018-01-26 11:28:34 -0700

    fixes #117 -- added left and right URN display

--- static/src/js/reader/widgets/passage-links.vue
+++ static/src/js/reader/widgets/passage-links.vue
@@ -2,7 +2,13 @@
   <widget>
     <span slot="header">CTS URN</span>
     <div slot="body">
-      <tt><a :href="`https://perseus-cts.eu1.eldarioncloud.com/api/cts?request=GetPassage&amp;urn=${passage.urn}`">{{ passage.urn.toString() }}</a></tt>
+      <template v-if="rightPassage">
+        <div>Left <tt><a :href="`${ctsApiUrl}?request=GetPassage&amp;urn=${rightPassage.urn}`">{{ leftPassage.urn.toString() }}</a></tt></div>
+        <div>Right <tt><a :href="`${ctsApiUrl}?request=GetPassage&amp;urn=${rightPassage.urn}`">{{ rightPassage.urn.toString() }}</a></tt></div>
+      </template>
+      <template v-else>
+        <tt><a :href="`${ctsApiUrl}?request=GetPassage&amp;urn=${passage.urn}`">{{ passage.urn.toString() }}</a></tt>
+      </template>
     </div>
   </widget>
 </template>
@@ -13,9 +19,20 @@ import widget from '../widget';
 
 export default {
   store,
+  data() {
+    return {
+      ctsApiUrl: 'https://perseus-cts.eu1.eldarioncloud.com/api/cts',
+    };
+  },
   computed: {
     passage() {
       return this.$store.getters['reader/passage'];
+    },
+    leftPassage() {
+      return this.$store.state.reader.leftPassage;
+    },
+    rightPassage() {
+      return this.$store.state.reader.rightPassage;
     },
   },
   components: {
