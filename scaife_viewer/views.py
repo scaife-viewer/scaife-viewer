5fa19fd8e759c8dea103f0bbc12cb886878c8e34
Brian Rosner <brian@brosner.com>
2018-02-19 10:06:03 -0700

    sped up top-level library page by removing an extra call

--- scaife_viewer/views.py
+++ scaife_viewer/views.py
@@ -33,27 +33,20 @@ class BaseLibraryView(View):
 
 class LibraryView(BaseLibraryView):
 
-    def get_text_groups(self):
-        return cts.text_inventory().text_groups()
-
     def as_html(self):
         return render(self.request, "library/index.html", {})
 
     def as_json(self):
         all_text_groups = cts.text_inventory().text_groups()
-
         text_groups = []
         works = []
         texts = []
-
         for text_group in all_text_groups:
             for work in text_group.works():
                 works.append(work)
                 for text in work.texts():
                     texts.append(text)
             text_groups.append(text_group)
-
-        text_groups = self.get_text_groups()
         payload = {
             "text_groups": [apify(text_group) for text_group in text_groups],
             "works": [apify(work) for work in works],
