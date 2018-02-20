7b2e233d9c70c7c1eed925fbb41abfdfc240c944
Brian Rosner <brian@brosner.com>
2018-02-20 13:23:04 -0700

    initial implementation of CHS commentary widget

---
+++ static/src/js/reader/widgets/chs-commentary.vue
@@ -0,0 +1,68 @@
+<template>
+  <widget class="word-list" v-if="show">
+    <span slot="header">CHS Commentary</span>
+    <div slot="body">
+      <p v-if="!comments || comments.length === 0">
+        No comments found.
+      </p>
+      <div v-else>
+        <div v-for="comment in comments" :key="comment._id">
+          <h6>{{ comment.latestRevision.title }}</h6>
+          <b>{{ comment.commenters[0].name }}</b>
+          <div v-html="comment.latestRevision.text"></div>
+        </div>
+      </div>
+    </div>
+  </widget>
+</template>
+
+<script>
+import qs from 'query-string';
+import store from '../../store';
+import widget from '../widget';
+
+export default {
+  store,
+  computed: {
+    passage() {
+      return this.$store.getters['reader/passage'];
+    },
+  },
+  data() {
+    return {
+      show: false,
+      comments: [],
+    };
+  },
+  mounted() {
+    this.fetchCommentary();
+  },
+  watch: {
+    passage: 'fetchCommentary',
+  },
+  methods: {
+    async fetchCommentary() {
+      const apiUrl = 'http://commentary.chs.orphe.us/graphql';
+      const { urn } = this.passage;
+      const query = `{ commentsOn(urn: "${urn}") { _id updated latestRevision { title text } commenters { _id name } } }`;
+      const params = qs.stringify({ query });
+      const res = await fetch(`${apiUrl}?${params}`);
+      if (!res.ok) {
+        if (res.status === 404) {
+          this.show = false;
+          return;
+        }
+        throw new Error(res.status);
+      }
+      if (this.show === false) {
+        this.show = true;
+      }
+      const data = await res.json();
+      this.comments = data.data.commentsOn;
+    },
+  },
+  components: {
+    widget,
+  },
+};
+</script>
