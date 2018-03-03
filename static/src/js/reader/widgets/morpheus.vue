1d4959bc77e6c585db7363e87ed2e3cdc1a448fc
James Tauber <jtauber@jtauber.com>
2018-03-02 22:33:09 -0500

    Morpheus instructions

--- static/src/js/reader/widgets/morpheus.vue
+++ static/src/js/reader/widgets/morpheus.vue
@@ -1,35 +1,38 @@
 <template>
   <widget class="morpheus">
     <span slot="header">Morpheus</span>
-    <div slot="body" v-if="morphBody">
-      <div class="group" v-for="group in morphBody" :key="group.uri">
-        <div class="head">
-          <span class="hdwd">{{ group.hdwd }}</span>
-          <span class="pofs-decl">{{ group.pofs }} {{ group.decl }}</span>
-        </div>
-        <div class="entries">
-          <div class="entry" v-for="entry in group.infl">
-            <div class="form">
-              <span class="stem">{{ entry.stem }}</span><span v-if="entry.suff" class="suff">-{{ entry.suff }}</span>
+    <div slot="body">
+      <div v-if="morphBody">
+        <div class="group" v-for="group in morphBody" :key="group.uri">
+          <div class="head">
+            <span class="hdwd">{{ group.hdwd }}</span>
+            <span class="pofs-decl">{{ group.pofs }} {{ group.decl }}</span>
+          </div>
+          <div class="entries">
+            <div class="entry" v-for="entry in group.infl">
+              <div class="form">
+                <span class="stem">{{ entry.stem }}</span><span v-if="entry.suff" class="suff">-{{ entry.suff }}</span>
+              </div>
+              <!-- {{ entry.pofs }} -->
+              <!-- {{ entry.stemtype }} -->
+              <div class="props">
+                {{ entry.tense }}
+                {{ entry.voice }}
+                {{ entry.mood }}
+                {{ entry.pers }}
+                {{ entry.case }}
+                {{ entry.num }}
+                {{ entry.gend }}
+                {{ entry.comp }}
+                <span v-if="entry.dial" class="dial">({{ entry.dial }})</span>
+              </div>
+              <!-- {{ entry.derivtype }} -->
+              <!-- {{ entry.morph }} -->
             </div>
-            <!-- {{ entry.pofs }} -->
-            <!-- {{ entry.stemtype }} -->
-            <div class="props">
-              {{ entry.tense }}
-              {{ entry.voice }}
-              {{ entry.mood }}
-              {{ entry.pers }}
-              {{ entry.case }}
-              {{ entry.num }}
-              {{ entry.gend }}
-              {{ entry.comp }}
-              <span v-if="entry.dial" class="dial">({{ entry.dial }})</span>
-            </div>
-            <!-- {{ entry.derivtype }} -->
-            <!-- {{ entry.morph }} -->
           </div>
         </div>
       </div>
+      <p v-else class="instructions">In <span class="mode">HIGHLIGHT</span> text mode, select a word to get morphological analysis (Greek only).</p>
     </div>
   </widget>
 </template>
