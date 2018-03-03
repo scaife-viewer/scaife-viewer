2b3a0033bbb0489ce36a8b3d597a21f9322421c0
James Tauber <jtauber@jtauber.com>
2018-03-02 22:55:08 -0500

    tweaked text search no results spacing

--- static/src/js/reader/widgets/search.vue
+++ static/src/js/reader/widgets/search.vue
@@ -17,7 +17,7 @@
         <div v-else-if="error"><small class="text-danger"><b>Error:</b> {{ error }}</small></div>
         <template v-else ref="items">
           <p class="instructions" v-if="query === '' && results.length === 0">Use text input above to find text in this version.</p>
-          <p v-else-if="results.length === 0"><small class="text-muted">No results found.</small></p>
+          <p class="no-results" v-else-if="results.length === 0">>No results found.</p>
           <ul v-else v-for="(r, idx) in results">
             <li :class="{ first: idx === 0, last: isLast(idx) }" :key="r.passage.urn">
               <router-link :to="toPassage(r.passage.urn)" :class="{ active : r.active }">{{ r.passage.refs.start.human_reference }}</router-link>
