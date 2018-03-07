ef4b342357cefdce2db4f792e6f1871dfd918b5b
James Tauber <jtauber@jtauber.com>
2018-03-07 18:16:31 -0500

    style morpheus no results more consistently

--- static/src/js/reader/widgets/morpheus.vue
+++ static/src/js/reader/widgets/morpheus.vue
@@ -33,7 +33,7 @@
           </div>
         </div>
       </div>
-      <p v-else-if="selectedWord" class="instructions">No results found.</p>
+      <p v-else-if="selectedWord" class="no-results">No results found.</p>
       <p v-else class="instructions">In <span class="mode">HIGHLIGHT</span> text mode, select a word to get morphological analysis (Greek only).</p>
     </div>
   </widget>
