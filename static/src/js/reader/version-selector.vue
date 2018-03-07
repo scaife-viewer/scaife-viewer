17bb80848ec552e42c719570e591c0ace45e8383
James Tauber <jtauber@jtauber.com>
2018-03-07 12:46:13 -0500

    version selector improvements

--- static/src/js/reader/version-selector.vue
+++ static/src/js/reader/version-selector.vue
@@ -8,12 +8,19 @@
     </div>
     <div :class="['version-option', 'dropdown-menu', { show }]">
       <router-link v-for="version in versions" :key="version.urn" class="dropdown-item" :to="to(version.urn)" @click.native="handleItemClick">
-        {{ version.label }}
-        <div class="metadata">{{ version.human_lang }} {{ version.kind }}</div>
+        <span v-if="version.urn === leftText.urn" class="side">L</span>
+        <span v-if="version.urn === rightText.urn" class="side">R</span>
+        <div>
+          <div class="label">
+            {{ version.label }}
+          </div>
+          <div class="description">{{ version.description }}</div>
+          <div class="metadata">{{ version.human_lang }} {{ version.kind }}</div>
+        </div>
       </router-link>
       <template v-if="remove">
         <div class="dropdown-divider"></div>
-        <router-link class="dropdown-item remove" :to="remove()" @click.native="handleRemoveClick">remove</router-link>
+        <router-link class="dropdown-item remove" :to="remove()" @click.native="handleRemoveClick">remove column</router-link>
       </template>
     </div>
   </div>
@@ -26,6 +33,14 @@ export default {
     return {
       show: false,
     };
+  },
+  computed: {
+    leftText() {
+      return this.$store.state.reader.leftText;
+    },
+    rightText() {
+      return this.$store.state.reader.rightText;
+    },
   },
   methods: {
     toggleMenu() {
