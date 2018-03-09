ed0490101fef16ca4279024500a38528b4750010
James Tauber <jtauber@jtauber.com>
2018-03-09 14:12:28 -0500

    passage redirect alert content and style tweaks

--- static/src/js/reader/passage-redirect-notice.vue
+++ static/src/js/reader/passage-redirect-notice.vue
@@ -1,5 +1,9 @@
 <template functional>
-  <div v-if="props.passage.redirected" class="alert alert-info text-info text-center" role="alert">
-    <b>{{ props.passage.redirected.previousUrn.reference }}</b> did not exist. Instead, we are showing you <b>{{ props.passage.urn.reference }}</b>.
+  <div v-if="props.passage.redirected" class="passage-redirect-alert" role="alert">
+    Passage reference
+    <b>{{ props.passage.redirected.previousUrn.reference }}</b>
+    could not be resolved.
+    Instead, we are showing you
+    <b>{{ props.passage.urn.reference }}</b>.
   </div>
 </template>
