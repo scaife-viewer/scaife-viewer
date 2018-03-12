adca94b017d424e15abd39709c6459035f229f66
Brian Rosner <brian@brosner.com>
2018-03-12 13:20:31 -0600

    worked around a MyCapytain bug in subreference handling

--- scaife_viewer/cts/__init__.py
+++ scaife_viewer/cts/__init__.py
@@ -21,12 +21,16 @@ def collection(urn: str) -> Collection:
     return resolve_collection(metadata.TYPE_URI)(URN(urn), metadata)
 
 
+def _has_subreference(reference):
+    return reference.parsed[0][2] is not None
+
+
 def _passage_urn_objs(urn: str):
     urn = URN(urn)
     if urn.reference is None:
         raise InvalidPassageReference("URN must contain a reference")
     reference = urn.reference
-    if reference.start.subreference or (reference.end and reference.end.subreference):
+    if _has_subreference(reference.start) or (reference.end and _has_subreference(reference.end)):
         raise InvalidPassageReference("URN must not contain a start or end subreference")
     urn = urn.upTo(URN.NO_PASSAGE)
     c = collection(urn)
