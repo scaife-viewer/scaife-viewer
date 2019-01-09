093407677db2f332ddfbcdafbea02fc64ed5c1a4
Patrick Altman <paltman@gmail.com>
2019-01-08 19:46:41 -0600

    We don't need two urn prefixes.  One will suffice.

--- static/src/js/router/routes.js
+++ static/src/js/router/routes.js
@@ -7,5 +7,5 @@ import Reader from '../reader';
 export default [
   { path: '/library', component: Library, name: 'library' },
   { path: '/library/:urn', component: CTSView, name: 'library_urn' },
-  { path: '/reader/urn::leftUrn', component: Reader, name: 'reader' },
+  { path: '/reader/:leftUrn', component: Reader, name: 'reader' },
 ];
