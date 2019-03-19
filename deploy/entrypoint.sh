b6dbf81407607c64c0ea3fd7b3f5e7ee7ed5ff4b
Jacob Wegner <jwegner@eldarion.com>
2019-03-19 09:25:36 -0500

    add sv-postgres and sv-elasticsearch services

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "Waiting for postgres..."
+
+while ! nc -z $SV_POSTGRES_HOST $SV_POSTGRES_PORT; do
+  sleep 0.1
+done
+echo "PostgreSQL started"
+
+
+echo "Waiting for elasticsearch..."
+while ! nc -z $SV_ELASTICSEARCH_HOST $SV_ELASTICSEARCH_PORT; do
+  sleep 0.1
+done
+
+# wait until the nodes endpoint comes online
+until $(curl --output /dev/null --silent http://$SV_ELASTICSEARCH_HOST:$SV_ELASTICSEARCH_PORT/_nodes/_all/http); do
+  sleep 1
+done
+echo "elasticsearch started"
+
+echo "Running migrations"
+python manage.py migrate
+
+exec "$@"
