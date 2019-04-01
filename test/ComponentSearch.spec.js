6d6bfd61b2e80437f043e1957167c238e9063cbb
Michael Herman <hermanmu@gmail.com>
2019-04-01 14:47:26 -0600

    add tests

---
+++ test/ComponentSearch.spec.js
@@ -0,0 +1,55 @@
+/* global describe, expect, it  */
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+
+import Search from '../static/src/js/library/search/Search.vue';
+import createStore from '../static/src/js/config';
+
+const localVue = createLocalVue();
+const router = new VueRouter();
+localVue.use(Vuex);
+localVue.use(VueRouter);
+
+
+describe('Search.vue', () => {
+  const config = createStore();
+  const store = new Vuex.Store(config);
+
+  it('sets the correct default data', () => {
+    const wrapper = shallowMount(Search, {
+      store,
+      localVue,
+      stubs: { BaseWidget: true },
+      router,
+    });
+
+    expect(wrapper.text()).toContain('Search Guide');
+    expect(wrapper.text()).toContain('Form');
+    expect(wrapper.text()).toContain('Lemma (Greek only)');
+  });
+
+  it('displays the search results correctly if no results are found', () => {
+    const wrapper = shallowMount(Search, {
+      store,
+      localVue,
+      stubs: { BaseWidget: true },
+      router,
+    });
+    wrapper.setData({
+      showSearchResults: true,
+      totalPages: 0,
+      pageNum: 1,
+      startIndex: 1,
+      endIndex: 10,
+      hasNext: false,
+      hasPrev: false,
+      totalResults: 0,
+      results: [],
+      textGroups: [],
+      secondLoading: false,
+      loading: false,
+    });
+    expect(wrapper.text()).toContain('No results found. Please try again.');
+  });
+});
