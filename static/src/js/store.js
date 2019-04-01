e404a61e71ca0828aaab9089f692c33d693dfabd
Michael Herman <hermanmu@gmail.com>
2019-04-01 10:05:36 -0600

    update text width change, add tests

--- static/src/js/store.js
+++ static/src/js/store.js
@@ -1,30 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import createPersistedState from 'vuex-persistedstate';
 
-import { library, reader } from './vuex';
+import createStore from './config';
 
 Vue.use(Vuex);
 
-const debug = process.env.NODE_ENV !== 'production';
-
-export default new Vuex.Store({
-  modules: {
-    library,
-    reader,
-  },
-  plugins: [
-    createPersistedState({
-      paths: [
-        'reader.sidebarLeftOpened',
-        'reader.sidebarRightOpened',
-        'reader.textSize',
-        'reader.textMode',
-        'reader.textWidth',
-      ],
-      storage: window.localStorage,
-    }),
-  ],
-  strict: true,
-  debug,
-});
+export default new Vuex.Store(createStore());
