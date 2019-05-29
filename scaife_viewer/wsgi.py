f58ee12c4e270876c8fe16a397dc7af0b6baebba
Jacob Wegner <jwegner@eldarion.com>
2019-05-29 14:18:48 -0500

    load text inventory and precompute library if DEBUG=False

--- scaife_viewer/wsgi.py
+++ scaife_viewer/wsgi.py
@@ -14,11 +14,18 @@ from django.core.wsgi import get_wsgi_application
 
 def setup():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scaife_viewer.settings")
-    from . import cts
-    # calling this will prime the cache in the master process. each fork
-    # will inherit it. gunicorn --preload is required for this to work.
-    cts.TextInventory.load()
-    print("Loaded text inventory")
+    from django.conf import settings
+
+    from . import cts, precomputed
+
+    if settings.DEBUG is False:
+        # calling this will prime the cache in the master process. each fork
+        # will inherit it. gunicorn --preload is required for this to work.
+        cts.TextInventory.load()
+        print("Loaded text inventory")
+
+        precomputed.library_view_json()
+        print("Precomputed library view JSON")
 
 
 def healthz(app):
