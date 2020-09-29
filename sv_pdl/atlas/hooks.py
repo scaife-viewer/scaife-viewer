d47fe8fa54c2c5be7fb7a37f559fe7933c92a31a
Jacob Wegner <jwegner@eldarion.com>
2020-09-29 09:33:36 -0500

    remove customized text group hook
    have fixed data upstream

--- sv_pdl/atlas/hooks.py
+++ sv_pdl/atlas/hooks.py
@@ -7,13 +7,3 @@ class ATLASHookSet(DefaultHookSet):
     def resolve_library(self):
         ti = text_inventory()
         return resolve_cts_collection_library(ti)
-
-    def extract_cts_text_group_metadata(self, text_group):
-        metadata = super().extract_cts_text_group_metadata(text_group)
-        # handle edge case where the group urn and version/work URNs differ
-        # https://github.com/PersDigUMD/PDL/blob/1d9ec2450d62874f60dc5fd9e6c55c7048c5a36c/data/salman/__cts__.xml
-        if metadata["urn"] == "urn:cts:perslit:sasalman:":
-            metadata["urn"] = "urn:cts:perslit:salman:"
-        return metadata
-
-    # FIXME: handle other issues within PDL repo
