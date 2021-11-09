2203d55f9807ac77e0caf65fee5327554efbb4a4
Jacob Wegner <jwegner@eldarion.com>
2021-11-08 18:36:33 -0600

    Prefer scaife-viewer/frontend widget

--- static/src/js/reader/widgets/WidgetNewAlexandriaCommentary.vue
+++ static/src/js/reader/widgets/WidgetNewAlexandriaCommentary.vue
@@ -2,64 +2,27 @@
   <base-widget class="new-alexandria-commentary" v-if="show">
     <span slot="header">New Alexandria Commentary</span>
     <div slot="body">
-      <p class="no-comment" v-if="!comments || comments.length === 0">
-        No comments found.
-      </p>
-      <div v-else>
-        <div v-for="comment in comments" :key="comment._id" class="comment">
-          <h6>{{ comment.latestRevision.title }}</h6>
-          <div class="commenter-name">{{ comment.commenters[0].name }}</div>
-          <div class="comment-text" v-html="comment.latestRevision.text"></div>
-        </div>
-      </div>
+      <new-alexandria-widget />
     </div>
   </base-widget>
 </template>
 
 <script>
-import qs from 'query-string';
+import NewAlexandriaWidget from '@scaife-viewer/widget-new-alexandria';
 
 export default {
-  name: 'widget-new-alexandria',
+  name: 'widget-attributions',
+  components: {
+    NewAlexandriaWidget,
+  },
   computed: {
     passage() {
       return this.$store.getters['reader/passage'];
     },
-  },
-  data() {
-    return {
-      show: false,
-      comments: [],
-    };
-  },
-  mounted() {
-    this.fetchCommentary();
-  },
-  watch: {
-    passage: {
-      handler: 'fetchCommentary',
-      immediate: true,
-    },
-  },
-  methods: {
-    async fetchCommentary() {
-      const apiUrl = 'https://chs-homer-proxy.herokuapp.com/homer-chs-proxy/graphql/';
-      const { urn } = this.passage;
-      const query = `{ commentsOn(urn: "${urn}") { _id updated latestRevision { title text } commenters { _id name } } }`;
-      const params = qs.stringify({ query });
-      const res = await fetch(`${apiUrl}?${params}`);
-      if (!res.ok) {
-        if (res.status === 404) {
-          this.show = false;
-          return;
-        }
-        throw new Error(res.status);
-      }
-      if (this.show === false) {
-        this.show = true;
-      }
-      const data = await res.json();
-      this.comments = data.data.commentsOn;
+    show() {
+      // TODO: Restore the previous functionality where we used a 404
+      // from the server to determine if the widget was shown or not.
+      return this.$store.getters['reader/passage'].urn.textGroup === 'tlg0012'
     },
   },
 };
@@ -71,3 +34,4 @@ export default {
   }
 }
 </style>
+
