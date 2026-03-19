66e4dc577546120983b69496924eb7829548bd2b
Charles Pletcher <cpletcher2@gmail.com>
2026-03-19 15:50:31 -0400

    Fix entrypoint.sh syntax

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -11,9 +11,6 @@ python manage.py slim_text_repos
 mkdir -p atlas_data
 python manage.py prepare_atlas_db --force
 
-ENV SV_ELASTICSEARCH_HOST=localhost
-ENV SV_ELASTICSEARCH_PORT=9200
-
 curl -O https://gist.githubusercontent.com/jacobwegner/68e538edf66539feb25786cc3c9cc6c6/raw/252e01a4c7e633b4663777a7e12dcb81119131e1/scaife-viewer-tmp.json
 curl -X PUT "http://${SV_ELASTICSEARCH_HOST}:${SV_ELASTICSEARCH_PORT}/_template/scaife-viewer?pretty" -H 'Content-Type: application/json' -d "$(cat scaife-viewer-tmp.json)"
 python manage.py indexer --max-workers=1 --limit=1000
