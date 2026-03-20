2c98231bc77d200199975ca2114ce444a69a8550
Jacob Wegner <jacobwegner@gmail.com>
2026-03-20 11:15:54 -0500

    Update for scaife-atlas-meta-repo

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
-mkdir -p ${CTS_LOCAL_DATA_PATH}
+SENTINEL_DIR=/sv-data/sentinels
+mkdir -p ${CTS_LOCAL_DATA_PATH} ${SENTINEL_DIR} ${ATLAS_DATA_DIR:-atlas_data}
 
+python manage.py migrate
 python manage.py loaddata sites
 python manage.py makemigrations
-python manage.py migrate sites
-python manage.py migrate
 
-python manage.py load_text_repos
-python manage.py slim_text_repos
+if [ ! -f ${SENTINEL_DIR}/.text_repos_loaded ]; then
+    python manage.py load_text_repos
+    python manage.py slim_text_repos
+    touch ${SENTINEL_DIR}/.text_repos_loaded
+fi
 
-mkdir -p atlas_data
-python manage.py prepare_atlas_db --force
+if [ ! -f ${SENTINEL_DIR}/.atlas_db_prepared ]; then
+    python manage.py prepare_atlas_db --force
+    touch ${SENTINEL_DIR}/.atlas_db_prepared
+fi
 
-curl -O https://gist.githubusercontent.com/jacobwegner/68e538edf66539feb25786cc3c9cc6c6/raw/252e01a4c7e633b4663777a7e12dcb81119131e1/scaife-viewer-tmp.json
-curl -X PUT "http://${SV_ELASTICSEARCH_HOST}:${SV_ELASTICSEARCH_PORT}/_template/scaife-viewer?pretty" -H 'Content-Type: application/json' -d "$(cat scaife-viewer-tmp.json)"
-python manage.py indexer --max-workers=1 --limit=1000
-
-rm scaife-viewer-tmp.json
+if [ ! -f ${SENTINEL_DIR}/.es_indexed ]; then
+    curl -X PUT "http://${SV_ELASTICSEARCH_HOST}:${SV_ELASTICSEARCH_PORT}/_template/scaife-viewer?pretty" -H 'Content-Type: application/json' -d "$(cat deploy/scaife-viewer-es-template.json)"
+    python manage.py indexer --max-workers=1 --limit=1000
+    touch ${SENTINEL_DIR}/.es_indexed
+fi
 
 exec "$@"
