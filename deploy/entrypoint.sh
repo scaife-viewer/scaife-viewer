41086984c6452d7746111bb6ae29d32fcdc21b57
Charles Pletcher <cpletcher2@gmail.com>
2026-04-01 14:51:06 -0400

    Try removing sentinel check to see if build works

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -7,21 +7,21 @@ python manage.py migrate
 python manage.py loaddata sites
 python manage.py makemigrations
 
-if [ ! -f ${SENTINEL_DIR}/.text_repos_loaded ]; then
-    python manage.py load_text_repos
-    python manage.py slim_text_repos
-    touch ${SENTINEL_DIR}/.text_repos_loaded
-fi
+# if [ ! -f ${SENTINEL_DIR}/.text_repos_loaded ]; then
+python manage.py load_text_repos
+python manage.py slim_text_repos
+touch ${SENTINEL_DIR}/.text_repos_loaded
+# fi
 
-if [ ! -f ${SENTINEL_DIR}/.atlas_db_prepared ]; then
-    python manage.py prepare_atlas_db --force
-    touch ${SENTINEL_DIR}/.atlas_db_prepared
-fi
+# if [ ! -f ${SENTINEL_DIR}/.atlas_db_prepared ]; then
+python manage.py prepare_atlas_db --force
+touch ${SENTINEL_DIR}/.atlas_db_prepared
+# fi
 
-if [ ! -f ${SENTINEL_DIR}/.es_indexed ]; then
-    curl -X PUT "http://${SV_ELASTICSEARCH_HOST}:${SV_ELASTICSEARCH_PORT}/_template/scaife-viewer?pretty" -H 'Content-Type: application/json' -d "$(cat deploy/scaife-viewer-es-template.json)"
-    python manage.py indexer --max-workers=1 --limit=1000
-    touch ${SENTINEL_DIR}/.es_indexed
-fi
+# if [ ! -f ${SENTINEL_DIR}/.es_indexed ]; then
+curl -X PUT "http://${SV_ELASTICSEARCH_HOST}:${SV_ELASTICSEARCH_PORT}/_template/scaife-viewer?pretty" -H 'Content-Type: application/json' -d "$(cat deploy/scaife-viewer-es-template.json)"
+python manage.py indexer --max-workers=1 --limit=1000
+touch ${SENTINEL_DIR}/.es_indexed
+# fi
 
 exec "$@"
